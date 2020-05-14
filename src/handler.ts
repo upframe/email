762bd3c3a0a20bad6ed809988a8e22a5f44bc45b
Mathis Bullinger <mathis@bullinger.dev>
2020-05-15 00:28:53 +0200

    fix: await execution

--- src/handler.ts
+++ src/handler.ts
@@ -4,20 +4,22 @@ import send from './send'
 export const email = async (event) => {
   if (process.env.IS_OFFLINE) event = { Records: [{ Sns: event }] }
   const records = event.Records ?? []
-  records.forEach(({ Sns }) => {
-    try {
-      const { action, ...rest } = JSON.parse(event.Records[0].Sns.Message)
-      if (typeof action !== 'string' || !(action in actions))
-        return logger.info(`unknown action ${action}, ignoring message`)
-      actions[action](rest)
-    } catch (e) {
-      logger.warn("couldn't parse message", { message: Sns?.Message })
-    }
-  })
+  await Promise.all(
+    records.map(({ Sns }) => {
+      try {
+        const { action, ...rest } = JSON.parse(event.Records[0].Sns.Message)
+        if (typeof action !== 'string' || !(action in actions))
+          return logger.info(`unknown action ${action}, ignoring message`)
+        return actions[action](rest)
+      } catch (e) {
+        logger.warn("couldn't parse message", { message: Sns?.Message })
+      }
+    })
+  )
 }
 
 const actions = {
-  SEND_EMAIL(info) {
-    send(info)
+  async SEND_EMAIL(info) {
+    await send(info)
   },
 }
